f7f2293869c716e8a2d152dffb4c8c453fe43a16
David Barton <david@Davids-MacBook-Pro.local>
2018-10-04 23:51:31 -0700

    liriBot Files

---
+++ liri.js
@@ -0,0 +1,103 @@
+//variables for required npms
+var keys = require("./keys.js");
+var request = require('request');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var fs = require('fs');
+//user interactions variables
+var client = new Twitter(keys.twitterKeys);
+var input = process.argv;
+var action = input[2];
+var inputs = input[3];
+//switch statement cases for "my-tweets", "spotify-this-song", "movie-this", do-what-it-says"
+switch (action) {
+	case "my-tweets":
+	twitter(inputs);
+	break;
+
+	case "spotify-this-song":
+	spotify(inputs);
+	break;
+
+	case "movie-this":
+	movie(inputs);
+	break;
+
+	case "do-what-it-says":
+	doit();
+	break;
+};
+
+function twitter(inputs) {
+	var params = {screen_name: inputs, count: 20};
+	
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+			if (!error) {
+				for (i = 0; i < tweets.length; i ++){
+					console.log("Tweet: " + "'" + tweets[i].text + "'" + " Created At: " + tweets[i].created_at);
+				}
+			} else {
+				console.log(error);
+			}
+		});
+
+}
+
+function spotify(inputs) {
+
+	var spotify = new Spotify(keys.spotifyKeys);
+		if (!inputs){
+        	inputs = 'The Sign';
+    	}
+		spotify.search({ type: 'track', query: inputs }, function(err, data) {
+			if (err){
+	            console.log('An error has occurred: ' + err);
+	            return;
+	        }
+
+	        var songInfo = data.tracks.items;
+	        console.log("Artist(s): " + songInfo[0].artists[0].name, "\nSong Name: " + songInfo[0].name, "\nPreview Link: " + songInfo[0].preview_url, "\nAlbum: " + songInfo[0].album.name);
+	});
+}
+
+
+function movie(inputs) {
+
+	var queryUrl = "http://www.omdbapi.com/?t=" + inputs + "&y=&plot=short&apikey=40e9cece";
+
+	request(queryUrl, function(error, response, body) {
+		if (!inputs){
+        	inputs = 'Mr Nobody';
+    	}
+		if (!error && response.statusCode === 200) {
+
+		    console.log("Title: " + JSON.parse(body).Title, "\nRelease Year: " + JSON.parse(body).Year, "\nIMDB Rating: " + JSON.parse(body).imdbRating, "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value, "\nCountry: " + JSON.parse(body).Country, "\nLanguage: " + JSON.parse(body).Language, "\nPlot: " + JSON.parse(body).Plot, "\nActors: " + JSON.parse(body).Actors);
+		}
+	});
+};
+
+function doit() {
+	fs.readFile('random.txt', "utf8", function(error, data){
+
+		if (error) {
+    		return console.log(error);
+  		}
+
+		// Then split it by commas (to make it more readable)
+		var dataArr = data.split(",");
+
+		// Each command is represented. Because of the format in the txt file, remove the quotes to run these commands. 
+		if (dataArr[0] === "spotify-this-song") {
+			var songcheck = dataArr[1].slice(1, -1);
+			spotify(songcheck);
+		} else if (dataArr[0] === "my-tweets") {
+			var tweetname = dataArr[1].slice(1, -1);
+			twitter(tweetname);
+		} else if(dataArr[0] === "movie-this") {
+			var movie_name = dataArr[1].slice(1, -1);
+			movie(movie_name);
+		} 
+		
+  	});
+
+};
